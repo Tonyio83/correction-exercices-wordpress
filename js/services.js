459831da0ecafa77e86b4d0159af0e4510fd65b9
Tonyio83 <anthonyfrancelle@gmail.com>
2020-01-16 13:41:18 +0100

    correction exercices wordpress 1 et 2

---
+++ js/services.js
@@ -0,0 +1,5 @@
+tarteaucitron.user.analyticsUa = 'UA-XXXXXXXX-X';
+tarteaucitron.user.analyticsMore = function () { /* add here your optionnal ga.push() */
+};
+(tarteaucitron.job = tarteaucitron.job || []).push('analytics');
+  
